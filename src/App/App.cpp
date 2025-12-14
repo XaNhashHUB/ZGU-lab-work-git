27c41e1ce7755ac2f60ff12e5db0eac4f96d570b
XaNhashHUB <game34874@gmail.com>
2025-12-14 14:21:38 +0300

    Update App.cpp
    Используется пространство имен std.
    Добавлена новая функция для вывода результата Summ.

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -1,13 +1,20 @@
 ﻿#include <iostream>
+using namespace std;
 
 // Функция сложения двух чисел
 int Summ(int num1, int num2) {
     return num1 + num2;
 }
 
+void PrintResult(int num1, int num2) {
+    cout << num1 << " + " << num2 << " = " << Summ(num1, num2) << endl;
+}
+
 int main()
 {
     std::cout << "Hello, World!\n";
+
+    PrintResult(5, 5);
 
     return 0;
 }
