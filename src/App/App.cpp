858a6eccde76f1689345f49f3686a8d40f111407
XaNhashHUB <game34874@gmail.com>
2025-12-14 14:25:49 +0300

    Update App.cpp
    Используется пространство имен std.
    Добавлена функция PrintSumm.
    Изменен вывод в консоль.

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -1,13 +1,20 @@
 ﻿#include <iostream>
+using namespace std;
 
 // Функция сложения двух чисел
 int Summ(int num1, int num2) {
     return num1 + num2;
 }
 
+void PrintSumm(int a, int b) {
+    cout << Summ(a, b);
+}
+
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello everybody!\n";
+
+    PrintSumm(1, 9);
 
     return 0;
 }
